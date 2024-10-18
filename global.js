7e21d3e46ca473bf1d6279d112d795afe3940119
chrisdscholz <153697137+chrisdscholz@users.noreply.github.com>
2024-10-18 13:48:56 -0700

    update

--- global.js
+++ global.js
@@ -4,19 +4,11 @@ function $$(selector, context = document) {
     return Array.from(context.querySelectorAll(selector));
 }
 
-// let navLinks = $$("nav a");
-
-// let currentLink = navLinks.find(
-//     (a) => a.host === location.host && a.pathname === location.pathname,
-// );
-
-// currentLink?.classList.add('current');
-
 let pages = [
-    {url: '', title: 'Home'},
-    {url: 'projects/', title: 'Projects'},
-    {url: 'contact/', title: 'Contact'},
-    {url: 'resume/', title: 'Resume'},
+    {url: '/', title: 'Home'},
+    {url: '/projects/', title: 'Projects'},
+    {url: '/contact/', title: 'Contact'},
+    {url: '/resume/', title: 'Resume'},
     {url: 'https://github.com/chrisdscholz', title: 'My GitHub Profile'},
 ];
 
@@ -30,17 +22,22 @@ for (let p of pages) {
     let title = p.title;
 
     if (!ARE_WE_HOME && !url.startsWith('http')) {
-        url = '../' + url;
+        url = '..' + url;
     }
-
-    //nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
 
     let a = document.createElement('a');
     a.href = url;
     a.textContent = title;
     nav.append(a);
 
-    if (a.host === location.host && a.pathname === location.pathname) {
+    let normPath = a.pathname === '/' || a.pathname === '/index.html' ? '/' : a.pathname;
+    let currPath = location.pathname === '/' || location.pathname === '/index.html' ? '/' : location.pathname;
+
+/*     if (a.host === location.host && a.pathname === location.pathname) {
+        a.classList.add('current');
+    } */
+
+    if (a.host === location.host && normPath === currPath) {
         a.classList.add('current');
     }
 
