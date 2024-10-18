2930affbb6baecb5e4febf1188be118207ee29f7
chrisdscholz <153697137+chrisdscholz@users.noreply.github.com>
2024-10-17 20:24:18 -0700

    update

--- global.js
+++ global.js
@@ -3,3 +3,29 @@ console.log("IT'S ALIVE!");
 function $$(selector, context = document) {
     return Array.from(context.querySelectorAll(selector));
 }
+
+// let navLinks = $$("nav a");
+
+// let currentLink = navLinks.find(
+//     (a) => a.host === location.host && a.pathname === location.pathname,
+// );
+
+// currentLink?.classList.add('current');
+
+let pages = [
+    {url: '', title: 'Home'},
+    {url: 'projects/', title: 'Projects'},
+    {url: 'contact/', title: 'Contact'},
+    {url: 'resume/', title: 'Resume'},
+    {url: 'https://github.com/chrisdscholz', title: 'My GitHub Profile'},
+];
+
+let nav = document.createElement('nav');
+document.body.prepend(nav);
+
+for (let p of pages) {
+    let url = p.url;
+    let title = p.title;
+
+    nav.insertAdjacentHTML('beforeend', '<a href="${url}">${title}</a>');
+}
